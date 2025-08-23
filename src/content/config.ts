5ce24424cdb9f461283711b9d8733b5f65d4afd9
audiovie <technique@audiovie.org>
2025-08-23 08:10:10 +0200

    front matter

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,16 +4,16 @@ const blogCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    sphere: z.string(),
+    category: z.string(), //sphere
+    place: z.string(),
+    year: z.number(),
     description: z.string().optional(),
     pubDate: z.date().optional().default(new Date('Thu, 01 Jan 1970 00:00:00 +0000')),
-    year: z.number().optional(),
     tags: z.array(z.string()).optional(),
-    category: z.string().optional(),
     author: z.string().optional(),
     source: z.string().optional(),
     index: z.number().optional(),
-    version: z.number().optional(),
+    version: z.string().optional().default("00"),
     featured: z.boolean().optional().default(false),
     draft: z.boolean().optional().default(false)
   })
--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -6,13 +6,13 @@ export async function GET() {
   const searchData = posts
     .filter(post => !post.data.draft)
     .map(post => ({
-      id: post.slug,
+      id: post.slug.split("/").slice(-1)[0],
       title: post.data.title,
       description: post.data.description,
       tags: post.data.tags || [],
       category: post.data.category || '',
       content: post.body,
-      url: `/blog/${post.slug}/`,
+      url: `/blog/${post.slug.split("/").slice(-1)[0]}/`,
     }));
 
   return new Response(JSON.stringify(searchData), {
