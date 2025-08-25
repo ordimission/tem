7c906ca2fd56ba49513cc096d9e61c3a76b9ec89
audiovie <technique@audiovie.org>
2025-08-25 08:31:20 +0200

    clean

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import basicAuth from './src/middleware/basic-auth';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-  middleware: [basicAuth],
+  integrations: []
 });
 
